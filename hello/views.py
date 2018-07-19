aba7f6d31576367938b08c71b2509a1e6f21fbad
Sing Swee Yang <sweeyangsing@gmail.com>
2018-07-19 17:20:46 +0800

    er

--- hello/views.py
+++ hello/views.py
@@ -4,12 +4,14 @@ from django.http import HttpResponse
 from .models import Greeting
 
 import requests
-
+import os
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
     # return render(request, 'index.html')
-    r = requests.get('http://httpbin.org/status/418')
+    times = int(os.environ.get('TIMES',3))
+    return HttpResponse('Hello!'* times)
+    requests.get('http://httpbin.org/status/418')
     print(r.text)
     return HttpResponse('<pre>' + r.text + '</pre>')
 
