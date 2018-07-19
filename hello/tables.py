afd700e950208aa9d19454b28db070ef2753b27d
Sing Swee Yang <sweeyangsing@gmail.com>
2018-07-19 18:28:25 +0800

    asd

--- hello/tables.py
+++ hello/tables.py
@@ -1,6 +1,5 @@
 import django_tables2 as tables
 from .models import Candidate
-from django_filters.views import FilterView
 from django_tables2.views import SingleTableMixin
 from django.utils.safestring import mark_safe
 from django.utils.html import escape
