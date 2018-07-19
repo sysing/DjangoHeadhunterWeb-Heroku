97221aec01ec94060b51d5d0b3c7394b5aa7212a
Sing Swee Yang <sweeyangsing@gmail.com>
2018-07-19 20:09:12 +0800

    adas

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -89,8 +89,7 @@ WSGI_APPLICATION = 'gettingstarted.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'default': dj_database_url.config()
     }
 }
 
