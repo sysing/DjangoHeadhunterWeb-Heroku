652ebdab8dc0027b3497b7f2809bd38f345c7824
Sing Swee Yang <sweeyangsing@gmail.com>
2018-07-19 18:54:56 +0800

    asd

--- hello/views.py
+++ hello/views.py
@@ -33,139 +33,139 @@ from django.utils import timezone
 from django.views.generic import TemplateView
 from chartjs.views.lines import BaseLineChartView
 
-
-class ActiveUsersJSONView(BaseLineChartView):
-
-    def get_labels(self):
-        days = self.request.GET.get('days',5)
-        days = int(days)
-        res = []
-        for x in range (0, days):
-            date = datetime.datetime.now() - datetime.timedelta(days = days - x -1)
-            # res.append(date.strftime("%a %b %d %H:%M:%S %Z %Y"))
-            res.append(date.strftime("%d %b "))
-        return res
-
-    def get_providers(self):
-        return ["活跃用户",]
-
-    def get_data(self):
-        days = self.request.GET.get('days',5)
-        days = int(days)
-        res = []
-        for x in range (0,days):
-            # time_min = datetime.datetime.now().replace(hour=8, minute=0, second=0, microsecond=0) - datetime.timedelta(days= days - x -1) #8hours for UTC+8
-            date = datetime.datetime.now() - datetime.timedelta(days = days - x - 1)
-            date_str = date.strftime('%Y-%m-%d')
-            active_user_count = Profile.objects.filter(last_activity__startswith = date_str).count()
-            res.append(active_user_count)
-        return [res,]
-
-active_users_json = ActiveUsersJSONView.as_view()
-
-class NewCandidatesJSONView(BaseLineChartView):
-
-    def get_labels(self):
-        res = []
-        days = 5
-        for x in range (0,days):
-            date = datetime.datetime.now() - datetime.timedelta(days = days - x - 1)
-            res.append(date.strftime("%d %b "))
-        return res
-
-    def get_providers(self):
-        return ["新增候选人",]
-
-    def get_data(self):
-        res = []
-        days = 5
-        for x in range (0,days):
-            time_min = datetime.datetime.now().replace(hour=8, minute=0, second=0, microsecond=0) - datetime.timedelta(days= days - x -1) #8hours for UTC+8
-            time_max = time_min + datetime.timedelta(days=1)
-            new_candidates_count = Candidate.objects.filter(ctime__gt = time_min,ctime__lt = time_max).count()
-            res.append(new_candidates_count)
-        return [res,]
-
-new_candidates_json = NewCandidatesJSONView.as_view()
-
-class TopCandidatesJSONView(BaseLineChartView   ):
-    candidates_jobs = Jobs_Candidates.objects.all()
-    job_count = {}
-    # for c in candidates_jobs:
-    #     if not  c.candidate_id in job_count:
-    #         job_count[c.candidate_id] = 1
-    #     else:
-    #         job_count[c.candidate_id] = job_count[c.candidate_id] + 1
-    sorted_keys = sorted(job_count, key=job_count.get,  reverse=True)[:5]
-
-    def get_labels(self):
-        label = []
-        for value in self.sorted_keys:
-            candidate = Candidate.objects.get(id = value)
-            label_name = candidate.__str__()
-            label.append(label_name)
-        return label
-
-    def get_providers(self):
-        return ["候选人岗位数",]
-
-    def get_data(self):
-        data = []
-        for k in self.sorted_keys:
-            data.append(self.job_count[k])
-        return [data,]
-
-top_candidates_json = TopCandidatesJSONView.as_view()
-
-class TopFunctionsJSONView(BaseLineChartView):
-    candidates_jobs = Jobs_Candidates.objects.all()
-    function_count = {}
-    for c in candidates_jobs:
-        function = c.candidate.function
-        if not  function.name in function_count:
-            function_count[function.name] = 1
-        else:
-            function_count[function.name] = function_count[function.name] + 1
-    sorted_keys = sorted(function_count, key=function_count.get,  reverse=True)[:5]
-
-    def get_labels(self):
-        return self.sorted_keys
-
-    def get_providers(self):
-        return ["职能岗位申请",]
-
-    def get_data(self):
-        data = []
-        for k in self.sorted_keys:
-            data.append(self.function_count[k])
-        return [data,]
-
-top_functions_json = TopFunctionsJSONView.as_view()
-
-
-class LineChartJSONView(BaseLineChartView):
-    def get_labels(self):
-        """Return 7 labels for the x-axis."""
-        return ["January", "February", "March", "April", "May", "June", "July"]
-
-    def get_providers(self):
-        """Return names of datasets."""
-        return ["Central", "Eastside", "Westside"]
-
-    def get_data(self):
-        """Return 3 datasets to plot."""
-
-        return [[75, 44, 92, 11, 44, 95, 35],
-                [41, 92, 18, 3, 73, 87, 92],
-                [87, 21, 94, 3, 90, 13, 65]]
-
-
-line_chart_json = LineChartJSONView.as_view()
-
-def dashboard(request):
-    context = {}
-    context['form'] = NameForm()
-    return render(request,'dashboard/dashboard.html',context)
+# 
+# class ActiveUsersJSONView(BaseLineChartView):
+#
+#     def get_labels(self):
+#         days = self.request.GET.get('days',5)
+#         days = int(days)
+#         res = []
+#         for x in range (0, days):
+#             date = datetime.datetime.now() - datetime.timedelta(days = days - x -1)
+#             # res.append(date.strftime("%a %b %d %H:%M:%S %Z %Y"))
+#             res.append(date.strftime("%d %b "))
+#         return res
+#
+#     def get_providers(self):
+#         return ["活跃用户",]
+#
+#     def get_data(self):
+#         days = self.request.GET.get('days',5)
+#         days = int(days)
+#         res = []
+#         for x in range (0,days):
+#             # time_min = datetime.datetime.now().replace(hour=8, minute=0, second=0, microsecond=0) - datetime.timedelta(days= days - x -1) #8hours for UTC+8
+#             date = datetime.datetime.now() - datetime.timedelta(days = days - x - 1)
+#             date_str = date.strftime('%Y-%m-%d')
+#             active_user_count = Profile.objects.filter(last_activity__startswith = date_str).count()
+#             res.append(active_user_count)
+#         return [res,]
+#
+# active_users_json = ActiveUsersJSONView.as_view()
+#
+# class NewCandidatesJSONView(BaseLineChartView):
+#
+#     def get_labels(self):
+#         res = []
+#         days = 5
+#         for x in range (0,days):
+#             date = datetime.datetime.now() - datetime.timedelta(days = days - x - 1)
+#             res.append(date.strftime("%d %b "))
+#         return res
+#
+#     def get_providers(self):
+#         return ["新增候选人",]
+#
+#     def get_data(self):
+#         res = []
+#         days = 5
+#         for x in range (0,days):
+#             time_min = datetime.datetime.now().replace(hour=8, minute=0, second=0, microsecond=0) - datetime.timedelta(days= days - x -1) #8hours for UTC+8
+#             time_max = time_min + datetime.timedelta(days=1)
+#             new_candidates_count = Candidate.objects.filter(ctime__gt = time_min,ctime__lt = time_max).count()
+#             res.append(new_candidates_count)
+#         return [res,]
+#
+# new_candidates_json = NewCandidatesJSONView.as_view()
+#
+# class TopCandidatesJSONView(BaseLineChartView   ):
+#     candidates_jobs = Jobs_Candidates.objects.all()
+#     job_count = {}
+#     for c in candidates_jobs:
+#         if not  c.candidate_id in job_count:
+#             job_count[c.candidate_id] = 1
+#         else:
+#             job_count[c.candidate_id] = job_count[c.candidate_id] + 1
+#     sorted_keys = sorted(job_count, key=job_count.get,  reverse=True)[:5]
+#
+#     def get_labels(self):
+#         label = []
+#         for value in self.sorted_keys:
+#             candidate = Candidate.objects.get(id = value)
+#             label_name = candidate.__str__()
+#             label.append(label_name)
+#         return label
+#
+#     def get_providers(self):
+#         return ["候选人岗位数",]
+#
+#     def get_data(self):
+#         data = []
+#         for k in self.sorted_keys:
+#             data.append(self.job_count[k])
+#         return [data,]
+#
+# top_candidates_json = TopCandidatesJSONView.as_view()
+#
+# class TopFunctionsJSONView(BaseLineChartView):
+#     candidates_jobs = Jobs_Candidates.objects.all()
+#     function_count = {}
+#     for c in candidates_jobs:
+#         function = c.candidate.function
+#         if not  function.name in function_count:
+#             function_count[function.name] = 1
+#         else:
+#             function_count[function.name] = function_count[function.name] + 1
+#     sorted_keys = sorted(function_count, key=function_count.get,  reverse=True)[:5]
+#
+#     def get_labels(self):
+#         return self.sorted_keys
+#
+#     def get_providers(self):
+#         return ["职能岗位申请",]
+#
+#     def get_data(self):
+#         data = []
+#         for k in self.sorted_keys:
+#             data.append(self.function_count[k])
+#         return [data,]
+#
+# top_functions_json = TopFunctionsJSONView.as_view()
+#
+#
+# class LineChartJSONView(BaseLineChartView):
+#     def get_labels(self):
+#         """Return 7 labels for the x-axis."""
+#         return ["January", "February", "March", "April", "May", "June", "July"]
+#
+#     def get_providers(self):
+#         """Return names of datasets."""
+#         return ["Central", "Eastside", "Westside"]
+#
+#     def get_data(self):
+#         """Return 3 datasets to plot."""
+#
+#         return [[75, 44, 92, 11, 44, 95, 35],
+#                 [41, 92, 18, 3, 73, 87, 92],
+#                 [87, 21, 94, 3, 90, 13, 65]]
+#
+#
+# line_chart_json = LineChartJSONView.as_view()
+#
+# def dashboard(request):
+#     context = {}
+#     context['form'] = NameForm()
+#     return render(request,'dashboard/dashboard.html',context)
 
 def index(request):
     # profile = Profile.objects.get(user=request.user)
