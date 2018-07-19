7e5dc61d0d225d94fd39116e5d717fc788e9b5d8
Sing Swee Yang <sweeyangsing@gmail.com>
2018-07-19 20:04:05 +0800

    adas

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -13,12 +13,15 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 import django_heroku
 import dj_database_url
+import psycopg2
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+DATABASE_URL = os.environ['DATABASE_URL']
 
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
