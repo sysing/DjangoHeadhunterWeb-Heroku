f4ee7122f3daa1c0ef6a2b23905eb63d0f0c9e22
Sing Swee Yang <sweeyangsing@gmail.com>
2018-07-20 11:55:18 +0800

    asd

--- hello/admin.py
+++ hello/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Function, Candidate, Industry, Province, Education, Profile, Job, Jobs_Candidates, Candidate_Status
+
+admin.site.register(Candidate)
+admin.site.register(Jobs_Candidates)
+admin.site.register(Function)
+admin.site.register(Industry)
+admin.site.register(Province)
+admin.site.register(Education)
+admin.site.register(Profile)
+admin.site.register(Job)
+admin.site.register(Candidate_Status)
