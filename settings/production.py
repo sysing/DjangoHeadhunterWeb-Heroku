0f28ca339997621aeca8996282b872851411adf6
Sing Swee Yang <sweeyangsing@gmail.com>
2018-07-25 11:01:11 +0800

    s

--- settings/production.py
+++ settings/production.py
@@ -1,15 +1,4 @@
 from .base import *
 
-DEBUG = True
-ALLOWED_HOSTS = ['*']
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'myproject',
-        'USER': 'myprojectuser',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
-}
+DEBUG = False
+ALLOWED_HOSTS = ['tranquil-retreat-52501.herokuapp.com']
