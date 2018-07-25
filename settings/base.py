964805f2d6d24e9e4e895692ecf965f7d44cacb3
Sing Swee Yang <sweeyangsing@gmail.com>
2018-07-25 13:00:00 +0800

    s

--- settings/base.py
+++ settings/base.py
@@ -78,13 +78,8 @@ WSGI_APPLICATION = 'gettingstarted.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
 # Password validation
@@ -135,5 +130,4 @@ STATICFILES_DIRS = (
 
 
 # Extra places for collectstatic to find static files.
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 django_heroku.settings(locals())
