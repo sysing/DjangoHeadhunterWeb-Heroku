77de7cfbb637642ae533e4e49b7db43f21986ff1
Sing Swee Yang <sweeyangsing@gmail.com>
2018-07-25 19:07:43 +0800

    s

--- settings/development.py
+++ settings/development.py
@@ -3,7 +3,12 @@ from .base import *
 DEBUG = True
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'matrix_hr_local',
+        'USER': 'postgres',
+        'PASSWORD': 'iamgroot',
+        'HOST': 'localhost', # Or something like this
+        'PORT': '5432',
     }
 }
+SECRET_KEY = 'g((h$7+$vvj=j@ecpj2rr(fkjl1)w$yuq(6&8=a#2y!bne4@v)'
